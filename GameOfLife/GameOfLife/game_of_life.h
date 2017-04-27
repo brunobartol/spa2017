1f945c6c83f54867839d721e0080c8f64b640751
Bruno Bartol <bruno.bartol1609@gmail.com>
2017-04-27 23:33:44 +0200

    Add files via upload

--- GameOfLife/GameOfLife/game_of_life.h
+++ GameOfLife/GameOfLife/game_of_life.h
@@ -10,10 +10,11 @@ class game_of_life {
 private:
 	static const unsigned int STUPACA = 40;
 	static const unsigned int REDAKA = 20;
-	bool _generacija[REDAKA][STUPACA];
-	bool _sljedeca_generacija[REDAKA][STUPACA];
-	bool slucajna_vrijednost();
-	bool celija_zauzeta(int i, int j);
+	char _generacija[REDAKA][STUPACA];
+	char _sljedeca_generacija[REDAKA][STUPACA];
+	int slucajna_vrijednost();
+	bool je_ziva(int i, int j);
+	int broj_zivih(int i, int j);
 
 public:
 	game_of_life();
